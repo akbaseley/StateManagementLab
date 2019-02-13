14a17c27fde2051bb87cfced84dd1565b9cb6e37
Anna B <akbaseley@gmail.com>
2019-02-13 15:59:18 -0500

    State Management Lab for C#.Net Jan 2019

--- State_Mangement_Lab/Controllers/HomeController.cs
+++ State_Mangement_Lab/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.SessionState;
+using State_Mangement_Lab.Models;
 
 namespace State_Mangement_Lab.Controllers
 {
     public class HomeController : Controller
     {
+        List<User> usersList = new List<User>();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +30,116 @@ namespace State_Mangement_Lab.Controllers
 
             return View();
         }
+
+        public ActionResult RegistrationPage()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterNewUser(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Session["userList"] is null)
+                {
+                    usersList.Add(newUser);
+                    Session["userList"] = usersList;
+                }
+                else
+                {
+                    usersList = (List<User>)(Session["userList"]);
+                    usersList.Add(newUser);
+                    Session["userList"] = usersList;
+                }
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Registration. Try again.";
+                return RedirectToAction("RegistrationPage");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginUser(string userName, string password)
+        {
+            if (Session["userList"] != null)
+            {
+                usersList = (List<User>)(Session["userList"]);
+                foreach (User user in usersList)
+                {
+                    if (user.UserName == userName)
+                    {
+                        if (user.Password != password)
+                        {
+                            ViewBag.ErrorMessage = "Invalid Password";
+                            return View("Login");
+                        }
+                        else
+                        {
+                            //create User session here
+                            Session["CurrentUser"] = user;
+                            return RedirectToAction("UserDetails");
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Invalid User Name and Password";
+                        return View("Login");
+                    }
+                }
+            }
+
+            ViewBag.ErrorMessage = "Please register before you log in.";
+            return View("Login");
+
+        }
+
+        public ActionResult UserDetails()
+        {
+            if (Session["CurrentUser"] is null)
+            {
+                ViewBag.ErrorMessage = "Please log in.";
+                return View("Login");
+            }
+            else
+            {
+                ViewBag.CurrentUser = Session["CurrentUser"];
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            if (Session["CurrentUser"] is null)
+            {
+                ViewBag.ErrorMessage = "You need to log in to log out!";
+                return View("Login");
+            }
+            else
+            {
+                User currentUser = (User)(Session["CurrentUser"]);
+                Session.Remove("CurrentUser");
+                ViewBag.UserName = currentUser.UserName;
+                return View();
+            }
+        }
+
+
+        /* HttpSessionState.Session["UserName"] = "John"; //stores session data
+
+        string str = HttpSessionState.Session["UserName"].ToString();
+
+        // Retrieves session data
+
+        HttpSessionState.Remove("Key to remove");
+
+        //Removes an object from the session state*/
+
+
     }
 }
---
+++ State_Mangement_Lab/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace State_Mangement_Lab.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+
+
+        public User(string userName, string email, string password, int age)
+        {
+            UserName = UserName;
+            Email = email;
+            Password = password;
+            Age = age;
+        }
+
+        public User() { }
+
+    }
+}
